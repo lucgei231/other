876db2c521f7588db54ace67363d24ba50e5c3be
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-02-17 17:36:49 +0000

    FIXED WEBAPP

--- saveasapp/script.js
+++ saveasapp/script.js
@@ -1,3 +1,20 @@
+document.getElementById('icon-btn').addEventListener('click', function() {
+    document.getElementById('icon-input').click();
+});
+
+document.getElementById('icon-input').addEventListener('change', function(event) {
+    var file = event.target.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var iconDataUrl = e.target.result;
+            localStorage.setItem('webapp_icon', iconDataUrl);
+            alert('Icon selected!');
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
 document.getElementById('save-btn').addEventListener('click', function () {
     var url = document.getElementById('url-input').value;
     if (!url.startsWith('http://') && !url.startsWith('https://')) {
