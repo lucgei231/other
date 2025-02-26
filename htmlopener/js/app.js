4f97bfe274031bdf545af9ee4d760d707c6595e8
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-02-26 17:54:16 +0000

    s

--- htmlopener/js/app.js
+++ htmlopener/js/app.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fileInput = document.getElementById('file-input');
+document.getElementById('file-input').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file && file.type === 'text/html') {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('display-area').innerHTML = e.target.result;
+        };
+        reader.readAsText(file);
+    } else {
+        alert('Please select a valid HTML file.');
+    }
+});
+
+document.getElementById('fullscreen-button').addEventListener('click', function() {
     const displayArea = document.getElementById('display-area');
-
-    fileInput.addEventListener('change', function(event) {
-        const file = event.target.files[0];
-
-        if (file) {
-            const reader = new FileReader();
-
-            reader.onload = function(e) {
-                displayArea.innerHTML = e.target.result;
-                // Execute embedded scripts
-                const scripts = displayArea.getElementsByTagName('script');
-                for (let i = 0; i < scripts.length; i++) {
-                    eval(scripts[i].innerText);
-                }
-            };
-
-            reader.readAsText(file);
-        } else {
-            alert('Please select a file.');
-        }
-    });
+    if (displayArea.requestFullscreen) {
+        displayArea.requestFullscreen();
+    } else if (displayArea.mozRequestFullScreen) { // Firefox
+        displayArea.mozRequestFullScreen();
+    } else if (displayArea.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        displayArea.webkitRequestFullscreen();
+    } else if (displayArea.msRequestFullscreen) { // IE/Edge
+        displayArea.msRequestFullscreen();
+    }
 });
