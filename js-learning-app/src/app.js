f257969e2a08429851d49504b04963f3e44ce266
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-02-28 17:14:17 +0000

    jjj

--- js-learning-app/src/app.js
+++ js-learning-app/src/app.js
@@ -1,8 +1,15 @@
+```javascript
 const lessons = [
     { title: "Lesson 1: Introduction to JavaScript", file: "lessons/lesson1.js" },
-    { title: "Lesson 2: Control Structures", file: "lessons/lesson2.js" },
+    { title: "Lesson 2: Variables and Data Types", file: "lessons/lesson2.js" },
     { title: "Lesson 3: Functions in JavaScript", file: "lessons/lesson3.js" },
-    // Add more lessons as needed
+    { title: "Lesson 4: Control Structures", file: "lessons/lesson4.js" },
+    { title: "Lesson 5: Arrays", file: "lessons/lesson5.js" },
+    { title: "Lesson 6: Objects", file: "lessons/lesson6.js" },
+    { title: "Lesson 7: DOM Manipulation", file: "lessons/lesson7.js" },
+    { title: "Lesson 8: Events", file: "lessons/lesson8.js" },
+    { title: "Lesson 9: Promises and Async/Await", file: "lessons/lesson9.js" },
+    { title: "Lesson 10: Error Handling", file: "lessons/lesson10.js" },
 ];
 
 function loadLesson(lesson) {
